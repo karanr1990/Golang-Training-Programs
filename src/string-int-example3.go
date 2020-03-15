91a8ac32be63b6692a8cbfb47bdad224cd211c38
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-16 01:37:52 +0530

    Create string-int-example3.go
    Here our input is provided in the form of “id:00211” where you have the prefix “id:” and the number is fixed 5 digits, padded with zeros if shorter, this is very quickly parsable. That is why we got 211 in the output.
    
    With this Sscanf() method, we must specify the format string to guide the parsing of the values.

---
+++ src/string-int-example3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    s := "id:00211"
+
+    var i int
+    if _, err := fmt.Sscanf(s, "id:%5d", &i); err == nil {
+        fmt.Println(i)
+    }
+}
