e11442b86a76888917bddd61b94763a11718dbc8
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-16 01:28:46 +0530

    Create string-int-example1.go

---
+++ src/string-int-example1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    data := "11"
+    if sv, err := strconv.Atoi(data); err == nil {
+        fmt.Printf("%T, %v", sv, sv)
+    }
+}
